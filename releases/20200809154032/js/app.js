4162263e7b276687b8cfa62b00907b9ec472c87d
YJLAugus <yesyjl@163.com>
2021-12-26 19:46:08 +0800

    add about

--- releases/20200809154032/js/app.js
+++ releases/20200809154032/js/app.js
@@ -107,7 +107,7 @@ webpackJsonp([1], {
                 }, {
                     name: "咦~",
                     path: o + "/img/face/bili/",
-                    maxNum: 26,
+                    maxNum: 50,
                     file: ".webp",
                     placeholder: "#bili_{alias}#"
                 }],
