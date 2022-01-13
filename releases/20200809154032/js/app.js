5be0ee84fb821dcf3b3873ea7edd8889ba468bb8
YJLAugus <yesyjl@163.com>
2022-01-13 20:09:45 +0800

    fixed emoji

--- releases/20200809154032/js/app.js
+++ releases/20200809154032/js/app.js
@@ -107,7 +107,7 @@ webpackJsonp([1], {
                 }, {
                     name: "咦~",
                     path: o + "/img/face/bili/",
-                    maxNum: 24,
+                    maxNum: 26,
                     file: ".webp",
                     placeholder: "#bili_{alias}#"
                 }],
